a4fafcd69075dee4080228601002cbc4a01505a9
Tomas <tomasbarredobusareli@gmail.com>
2019-09-30 22:39:19 -0300

    cuarto commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose =  require('mongoose');
+const db = 'mongodb://localhost/trivia';
+mongoose.connect (db, {useNewUrlParser:true})
+ .then(()=> {
+console.log('conectado');
+})
+.catch (err =>console.log(err));
 const port = process.env.PORT || 3000;
 const app = express();
+app.set('view engine','pug');
+app.set('views','./views');
 app.get ('/',(req,res)=>{
-  res.send('jeje');
+  res.render('index',{
+    home:'true'
+  });
 });
 app.listen(port);
